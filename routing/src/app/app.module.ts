e90ee0c15b533124ffc68ac37f03f6e97759cabd
preethisheks4m <85828602+preethisheks4m@users.noreply.github.com>
2022-01-07 18:19:08 +0530

    components added

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProductsComponent } from './products/products.component';
+import { TermsComponent } from './terms/terms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent,
+    ProductsComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule
